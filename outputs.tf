571974bef61fe43d38f2f7fe357ca9415580ca60
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "routes" {
-  description = "All route resources"
-  value       = azurerm_route.routes
-}
 output "routes_address_prefix" {
-  description = "List of address_prefix values across all routes"
-  value       = [for k, v in azurerm_route.routes : v.address_prefix]
+  description = "Map of address_prefix values across all routes, keyed the same as var.routes"
+  value       = { for k, v in azurerm_route.routes : k => v.address_prefix }
 }
 output "routes_name" {
-  description = "List of name values across all routes"
-  value       = [for k, v in azurerm_route.routes : v.name]
+  description = "Map of name values across all routes, keyed the same as var.routes"
+  value       = { for k, v in azurerm_route.routes : k => v.name }
 }
 output "routes_next_hop_in_ip_address" {
-  description = "List of next_hop_in_ip_address values across all routes"
-  value       = [for k, v in azurerm_route.routes : v.next_hop_in_ip_address]
+  description = "Map of next_hop_in_ip_address values across all routes, keyed the same as var.routes"
+  value       = { for k, v in azurerm_route.routes : k => v.next_hop_in_ip_address }
 }
 output "routes_next_hop_type" {
-  description = "List of next_hop_type values across all routes"
-  value       = [for k, v in azurerm_route.routes : v.next_hop_type]
+  description = "Map of next_hop_type values across all routes, keyed the same as var.routes"
+  value       = { for k, v in azurerm_route.routes : k => v.next_hop_type }
 }
 output "routes_resource_group_name" {
-  description = "List of resource_group_name values across all routes"
-  value       = [for k, v in azurerm_route.routes : v.resource_group_name]
+  description = "Map of resource_group_name values across all routes, keyed the same as var.routes"
+  value       = { for k, v in azurerm_route.routes : k => v.resource_group_name }
 }
 output "routes_route_table_name" {
-  description = "List of route_table_name values across all routes"
-  value       = [for k, v in azurerm_route.routes : v.route_table_name]
+  description = "Map of route_table_name values across all routes, keyed the same as var.routes"
+  value       = { for k, v in azurerm_route.routes : k => v.route_table_name }
 }
 
